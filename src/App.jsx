6cd1fea2d90323c94cf91879ae3a3583676ec0fd
Sahil Mahapatra <sahilm231199@gmail.com>
2023-11-25 23:01:20 +0530

    showing images in grid like structure and user prompts one by one

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [prompts, setPrompts] = useState(Array(10).fill(""));
   const [images, setImages] = useState(Array(10).fill(null));
+  const [currentStep, setCurrentStep] = useState(0);
 
-  function handlePromptChange(index, value) {
+  useEffect(() => {
+    if (currentStep === 10) {
+      fetchImages();
+    }
+  }, [currentStep]);
+
+  const handlePromptChange = (value) => {
     const newPrompts = [...prompts];
-    newPrompts[index] = value;
+    newPrompts[currentStep] = value;
     setPrompts(newPrompts);
-  }
+  };
 
-  async function handleSubmit() {
+  const handleSubmitPrompt = () => {
+    if (currentStep < 9) {
+      setCurrentStep((prevStep) => prevStep + 1);
+    } else {
+      setCurrentStep(10);
+    }
+  };
+
+  const fetchImages = async () => {
     const imagePromises = prompts.map((prompt) => search({ inputs: prompt }));
 
     try {
@@ -23,7 +38,7 @@ function App() {
     } catch (error) {
       console.error("Error fetching images:", error);
     }
-  }
+  };
 
   async function search(data) {
     const response = await fetch(
@@ -48,36 +63,40 @@ function App() {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-6">Awesome React App</h1>
 
-        <form>
-          {prompts.map((prompt, index) => (
-            <div key={index} className="mb-2">
-              <input
-                type="text"
-                value={prompt}
-                onChange={(e) => handlePromptChange(index, e.target.value)}
-                placeholder={`Enter prompt ${index + 1}`}
-                className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              />
-            </div>
-          ))}
-          <button
-            type="button"
-            onClick={handleSubmit}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none hover:bg-blue-600"
-          >
-            Submit
-          </button>
-        </form>
+        {currentStep < 10 && (
+          <div>
+            <label htmlFor={`prompt-${currentStep + 1}`} className="sr-only">
+              {`Enter prompt ${currentStep + 1}`}
+            </label>
+            <input
+              type="text"
+              id={`prompt-${currentStep + 1}`}
+              value={prompts[currentStep]}
+              onChange={(e) => handlePromptChange(e.target.value)}
+              placeholder={`Enter prompt ${currentStep + 1}`}
+              className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300 mb-2"
+            />
+            <button
+              type="button"
+              onClick={handleSubmitPrompt}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none hover:bg-blue-600"
+            >
+              {currentStep === 9 ? "Submit" : "Next"}
+            </button>
+          </div>
+        )}
 
-        {images.every((image) => image !== null) && (
-          <div className="mt-4">
+        {currentStep === 10 && images.every((image) => image !== null) && (
+          <div className="grid grid-cols-5 gap-4 mt-4">
             {images.map((imageUrl, index) => (
-              <img
-                key={index}
-                src={imageUrl}
-                alt={`Resulting Image ${index + 1}`}
-                className="max-w-full mb-2"
-              />
+              <div key={index} className="comic-plate">
+                <img
+                  src={imageUrl}
+                  alt={`Resulting Image ${index + 1}`}
+                  className="max-w-full mb-2"
+                />
+                <p className="text-center text-gray-300">{prompts[index]}</p>
+              </div>
             ))}
           </div>
         )}
