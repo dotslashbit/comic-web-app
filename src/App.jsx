90a1f5ae65e25a12be74f50b0acf8f654c87389d
Sahil Mahapatra <sahilm231199@gmail.com>
2023-11-26 00:53:40 +0530

    added loading animation and added validation for user input, also included back button for user prompt

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [currentStep, setCurrentStep] = useState(0);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     if (currentStep === 10) {
@@ -21,6 +22,13 @@ function App() {
   };
 
   const handleSubmitPrompt = () => {
+    if (!prompts[currentStep].trim()) {
+      setError("Please enter a prompt before moving forward.");
+      return;
+    }
+
+    setError(null);
+
     if (currentStep < 9) {
       setCurrentStep((prevStep) => prevStep + 1);
     } else {
@@ -28,9 +36,21 @@ function App() {
     }
   };
 
+  const goBack = () => {
+    if (currentStep > 0) {
+      setCurrentStep((prevStep) => prevStep - 1);
+    }
+  };
+
   const fetchImages = async () => {
     try {
       setIsLoading(true);
+
+      // Check if the user is online
+      if (!navigator.onLine) {
+        throw new Error("No internet connection. Please check your network.");
+      }
+
       const imagePromises = prompts.map((prompt) => search({ inputs: prompt }));
       const imageResponses = await Promise.all(imagePromises);
 
@@ -40,7 +60,7 @@ function App() {
       setImages(imageUrls);
     } catch (error) {
       console.error("Error fetching images:", error);
-      setError("Error fetching images. Please try again.");
+      setError(error.message || "Error fetching images. Please try again.");
     } finally {
       setIsLoading(false);
     }
@@ -78,12 +98,34 @@ function App() {
     completed: index < currentStep,
   }));
 
+  const shareImages = () => {
+    const dataUrls = images.map((blob) => {
+      const reader = new FileReader();
+      return new Promise((resolve) => {
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+      });
+    });
+
+    Promise.all(dataUrls).then((dataUrls) => {
+      const imagesText = dataUrls.join("\n");
+      navigator.clipboard.writeText(imagesText);
+      setCopied(true);
+    });
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-gray-800 to-black text-white font-sans">
-      <div className="text-center">
+      <div className="container mx-auto text-center">
         <h1 className="text-4xl font-bold mb-6">Awesome React App</h1>
 
-        {isLoading && <p className="text-gray-300">Loading...</p>}
+        {isLoading && (
+          <div className="flex items-center justify-center">
+            <div className="spinner border-t-4 border-blue-500 border-solid h-12 w-12 rounded-full animate-spin"></div>
+          </div>
+        )}
 
         {error && <p className="text-red-500 mb-4">{error}</p>}
 
@@ -98,15 +140,22 @@ function App() {
               value={prompts[currentStep]}
               onChange={(e) => handlePromptChange(e.target.value)}
               placeholder={`Enter prompt ${currentStep + 1}`}
-              className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300 mb-2 w-full"
+              className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300 mb-2 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3"
             />
             <p className="text-gray-500 text-sm mt-2 mb-4">
               Provide a concise description for better results.
             </p>
             <button
               type="button"
+              onClick={goBack}
+              className="bg-gray-500 text-white px-4 py-2 rounded-md focus:outline-none hover:bg-gray-600 mr-2"
+            >
+              Back
+            </button>
+            <button
+              type="button"
               onClick={handleSubmitPrompt}
-              className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none hover:bg-blue-600"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none hover:bg-blue-600 w-full sm:w-auto"
             >
               {currentStep === 9 ? "Submit" : "Next"}
             </button>
@@ -131,7 +180,7 @@ function App() {
         )}
 
         {currentStep === 10 && images.every((image) => image !== null) && (
-          <div className="grid grid-cols-5 gap-4 mt-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-4">
             {images.map((imageUrl, index) => (
               <div key={index} className="comic-plate">
                 <img
@@ -142,6 +191,19 @@ function App() {
                 <p className="text-center text-gray-300">{prompts[index]}</p>
               </div>
             ))}
+            <div className="mt-4">
+              <button
+                onClick={shareImages}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none hover:bg-blue-600"
+              >
+                Share Images
+              </button>
+              {copied && (
+                <p className="text-green-500 mt-2">
+                  Images copied to clipboard!
+                </p>
+              )}
+            </div>
           </div>
         )}
       </div>
