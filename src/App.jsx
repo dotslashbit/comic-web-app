a72381b38c1e281ad5264da7c388ceb10ddd4669
Sahil Mahapatra <sahilm231199@gmail.com>
2023-11-26 14:17:03 +0530

    added functionality to download generated images in a zip file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import "./App.css"; // Import your CSS file for styling
+import "./App.css";
+import JSZip from "jszip";
+import FileSaver from "file-saver";
 
 function App() {
   const [prompts, setPrompts] = useState(Array(10).fill(""));
@@ -98,21 +100,21 @@ function App() {
     completed: index < currentStep,
   }));
 
-  const shareImages = () => {
-    const dataUrls = images.map((blob) => {
-      const reader = new FileReader();
-      return new Promise((resolve) => {
-        reader.onloadend = () => {
-          resolve(reader.result);
-        };
-        reader.readAsDataURL(blob);
+  const downloadImages = () => {
+    const zip = new JSZip();
+    const imagePromises = images.map((imageUrl, index) => {
+      const fileName = `Resulting_Image_${index + 1}.png`;
+      return fetch(imageUrl).then((response) => {
+        return response.blob().then((blob) => {
+          zip.file(fileName, blob);
+        });
       });
     });
 
-    Promise.all(dataUrls).then((dataUrls) => {
-      const imagesText = dataUrls.join("\n");
-      navigator.clipboard.writeText(imagesText);
-      setCopied(true);
+    Promise.all(imagePromises).then(() => {
+      zip.generateAsync({ type: "blob" }).then((content) => {
+        FileSaver.saveAs(content, "images.zip");
+      });
     });
   };
 
@@ -183,20 +185,26 @@ function App() {
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-4">
             {images.map((imageUrl, index) => (
               <div key={index} className="comic-plate">
-                <img
-                  src={imageUrl}
-                  alt={`Resulting Image ${index + 1}`}
+                <a
+                  href={imageUrl}
+                  download={`Resulting_Image_${index + 1}.png`}
                   className="max-w-full mb-2"
-                />
+                >
+                  <img
+                    src={imageUrl}
+                    alt={`Resulting Image ${index + 1}`}
+                    className="max-w-full mb-2"
+                  />
+                </a>
                 <p className="text-center text-gray-300">{prompts[index]}</p>
               </div>
             ))}
             <div className="mt-4">
               <button
-                onClick={shareImages}
+                onClick={downloadImages}
                 className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none hover:bg-blue-600"
               >
-                Share Images
+                Download Images
               </button>
               {copied && (
                 <p className="text-green-500 mt-2">
