76dbb9c90898decf49a28881eac369928d033559
Sahil Mahapatra <sahilm231199@gmail.com>
2023-11-26 15:57:29 +0530

    added text hint on how to add text annotation

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,9 @@ function App() {
               className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300 mb-2 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3"
             />
             <p className="text-gray-500 text-sm mt-2 mb-4">
-              Provide a concise description for better results.
+              Provide a concise description for better results. <br />
+              You can include text annotations like "dog with a candy with a
+              text annotation "I'm happy".
             </p>
             <button
               type="button"
